d1d9f154e39bdf89cfc7b1493688ccdf0edb38f6
530farm <brentadamluna@gmail.com>
2016-05-14 17:53:37 -0700

    added long practice test

---
+++ a01-prep-master/skeleton/lib/assessment01.rb
@@ -0,0 +1,117 @@
+class Array
+  def my_inject(accumulator = nil)
+
+  end
+end
+
+def is_prime?(num)
+end
+
+def primes(count)
+
+end
+
+# the "calls itself recursively" spec may say that there is no method
+# named "and_call_original" if you are using an older version of
+# rspec. You may ignore this failure.
+# Also, be aware that the first factorial number is 0!, which is defined
+# to equal 1. So the 2nd factorial is 1!, the 3rd factorial is 2!, etc.
+def factorials_rec(num)
+
+end
+
+class Array
+  def dups
+
+  end
+end
+
+class String
+  def symmetric_substrings
+
+  end
+end
+
+class Array
+  def merge_sort(&prc)
+
+  end
+
+  private
+  def self.merge(left, right, &prc)
+
+  end
+end
+
+class Array
+  def bubble_sort(&blk)
+
+  end
+
+  def bubble_sort!(&blk)
+
+  end
+end
+
+# ### My Each
+# Extend the Array class to include a method named `my_each` that takes a
+# block, calls the block on every element of the array, and then returns
+# the original array. Do not use Enumerable's `each` method. I want to be
+# able to write:
+#
+# ```ruby
+# # calls my_each twice on the array, printing all the numbers twice.
+# return_value = [1, 2, 3].my_each do |num|
+#   puts num
+# end.my_each do |num|
+#   puts num
+# end
+# # => 1
+#      2
+#      3
+#      1
+#      2
+#      3
+#
+# p return_value # => [1, 2, 3]
+# ```
+
+class Array
+  def my_each(&prc)
+  
+  end
+end
+
+# ### My Enumerable Methods
+# * Implement new `Array` methods `my_map` and `my_select`. Do
+#   it by monkey-patching the `Array` class. Don't use any of the
+#   original versions when writing these. Use your `my_each` method to
+#   define the others. Remember that `each`/`map`/`select` do not modify
+#   the original array.
+# * Implement a `my_inject` method. Your version shouldn't take an
+#   optional starting argument; just use the first element. Ruby's
+#   `inject` is fancy (you can write `[1, 2, 3].inject(:+)` to shorten
+#   up `[1, 2, 3].inject { |sum, num| sum + num }`), but do the block
+#   (and not the symbol) version. Again, use your `my_each` to define
+#   `my_inject`. Again, do not modify the original array.
+
+class Array
+  def my_map(&prc)
+
+  end
+
+  def my_select(&prc)
+
+  end
+
+
+end
+
+# ### Concatenate
+# Create a method that takes in an `Array` of `String`s and uses `inject`
+# to return the concatenation of the strings.
+#
+# ```ruby
+# concatenate(["Yay ", "for ", "strings!"])
+# # => "Yay for strings!"
+# ```
